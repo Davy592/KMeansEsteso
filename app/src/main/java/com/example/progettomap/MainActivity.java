d61ee1d27959d21a24563e3ac5fde496b4315d2a
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-24 18:30:00 +0200

    Risolti ulteriori warnings

--- app/src/main/java/com/example/progettomap/MainActivity.java
+++ app/src/main/java/com/example/progettomap/MainActivity.java
@@ -32,94 +32,82 @@ public class MainActivity extends AppCompatActivity {
         EditText tbServer, tbPort, tbDatabase, tbTable, tbUser, tbPassword, tbK;
         Button btConnect, btCalc;
         LinearLayout llK;
-        chDef = (CheckBox) findViewById(R.id.chDef);
-        tbServer = (EditText) findViewById(R.id.tbServer);
-        tbPort = (EditText) findViewById(R.id.tbPort);
-        tbDatabase = (EditText) findViewById(R.id.tbDatabase);
-        tbTable = (EditText) findViewById(R.id.tbTable);
-        tbUser = (EditText) findViewById(R.id.tbUser);
-        tbPassword = (EditText) findViewById(R.id.tbPassword);
-        tbK = (EditText) findViewById(R.id.tbK);
-        btConnect = (Button) findViewById(R.id.btConnect);
-        btCalc = (Button) findViewById(R.id.btCalc);
-        llK = (LinearLayout) findViewById(R.id.llK);
+        chDef = findViewById(R.id.chDef);
+        tbServer = findViewById(R.id.tbServer);
+        tbPort = findViewById(R.id.tbPort);
+        tbDatabase = findViewById(R.id.tbDatabase);
+        tbTable = findViewById(R.id.tbTable);
+        tbUser = findViewById(R.id.tbUser);
+        tbPassword = findViewById(R.id.tbPassword);
+        tbK = findViewById(R.id.tbK);
+        btConnect = findViewById(R.id.btConnect);
+        btCalc = findViewById(R.id.btCalc);
+        llK = findViewById(R.id.llK);
 
-        chDef.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (chDef.isChecked()) {
-                    tbServer.setEnabled(false);
-                    tbPort.setEnabled(false);
-                    tbDatabase.setEnabled(false);
-                    tbTable.setEnabled(false);
-                    tbUser.setEnabled(false);
-                    tbPassword.setEnabled(false);
-                    tbServer.setText("localhost");
-                    tbPort.setText("3306");
-                    tbDatabase.setText("MapDB");
-                    tbTable.setText("playtennis");
-                    tbUser.setText("MapUser");
-                    tbPassword.setText("map");
+        chDef.setOnClickListener(v -> {
+            if (chDef.isChecked()) {
+                tbServer.setEnabled(false);
+                tbPort.setEnabled(false);
+                tbDatabase.setEnabled(false);
+                tbTable.setEnabled(false);
+                tbUser.setEnabled(false);
+                tbPassword.setEnabled(false);
+                tbServer.setText("localhost");
+                tbPort.setText("3306");
+                tbDatabase.setText("MapDB");
+                tbTable.setText("playtennis");
+                tbUser.setText("MapUser");
+                tbPassword.setText("map");
+            } else {
+                tbServer.setEnabled(true);
+                tbPort.setEnabled(true);
+                tbDatabase.setEnabled(true);
+                tbTable.setEnabled(true);
+                tbUser.setEnabled(true);
+                tbPassword.setEnabled(true);
+                tbServer.setText("");
+                tbPort.setText("");
+                tbDatabase.setText("");
+                tbTable.setText("");
+                tbUser.setText("");
+                tbPassword.setText("");
+            }
+        });
+
+
+        btConnect.setOnClickListener(v -> {
+            try {
+                if (tbServer.getText().toString().equals("") || tbPort.getText().toString().equals("") || tbDatabase.getText().toString().equals("") || tbTable.getText().toString().equals("") || tbUser.getText().toString().equals("") || tbPassword.getText().toString().equals("")) {
+                    openDialog("ERRORE", "Inserire tutti i campi!");
                 } else {
-                    tbServer.setEnabled(true);
-                    tbPort.setEnabled(true);
-                    tbDatabase.setEnabled(true);
-                    tbTable.setEnabled(true);
-                    tbUser.setEnabled(true);
-                    tbPassword.setEnabled(true);
-                    tbServer.setText("");
-                    tbPort.setText("");
-                    tbDatabase.setText("");
-                    tbTable.setText("");
-                    tbUser.setText("");
-                    tbPassword.setText("");
+                    llK.setVisibility(View.INVISIBLE);
+                    btCalc.setVisibility(View.INVISIBLE);
+                    data = new Data(tbServer.getText().toString(), Integer.parseInt(tbPort.getText().toString()), tbDatabase.getText().toString(), tbTable.getText().toString(), tbUser.getText().toString(), tbPassword.getText().toString());
+                    System.out.println("Provo");
+                    System.out.println("Data: " + data);
+                    llK.setVisibility(View.VISIBLE);
+                    btCalc.setVisibility(View.VISIBLE);
+                    System.out.println("fatto");
+                    openDialog("DATI CARICATI", data.toString());
                 }
+            } catch (DatabaseConnectionException | SQLException | NoValueException | EmptySetException e) {
+                System.out.println("Ho preso " + e.getMessage());
+                openDialog("ERRORE", e.getMessage());
             }
         });
 
-
-        btConnect.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                /*System.out.println("tbServer: " + tbServer.getText());
-                System.out.println("tbPort: " + tbPort.getText());
-                System.out.println("tbDatabase: " + tbDatabase.getText());
-                System.out.println("tbTable: " + tbTable.getText());
-                System.out.println("tbUser: " + tbUser.getText());
-                System.out.println("tbPassword: " + tbPassword.getText());*/
-                try {
-                    if (tbServer.getText().toString().equals("") || tbPort.getText().toString().equals("") || tbDatabase.getText().toString().equals("") || tbTable.getText().toString().equals("") || tbUser.getText().toString().equals("") || tbPassword.getText().toString().equals("")) {
-                        openDialog("ERRORE", "Inserire tutti i campi!");
-                    } else {
-                        llK.setVisibility(View.INVISIBLE);
-                        btCalc.setVisibility(View.INVISIBLE);
-                        data = new Data(tbServer.getText().toString(), Integer.parseInt(tbPort.getText().toString()), tbDatabase.getText().toString(), tbTable.getText().toString(), tbUser.getText().toString(), tbPassword.getText().toString());
-                        System.out.println("Provo");
-                        System.out.println("Data: " + data);
-                        llK.setVisibility(View.VISIBLE);
-                        btCalc.setVisibility(View.VISIBLE);
-                        System.out.println("fatto");
-                        openDialog("DATI CARICATI", data.toString());
-                    }
-                } catch (DatabaseConnectionException | SQLException | NoValueException | EmptySetException e) {
-                    System.out.println("Ho preso " + e.getMessage());
-                    openDialog("ERRORE", e.getMessage());
+        btCalc.setOnClickListener(v -> {
+            int numIter;
+            try {
+                if (tbK.toString().equals("")) {
+                    openDialog("ERRORE", "Inserire il numero di cluster!");
+                } else {
+                    kmeans = new KMeansMiner(Integer.parseInt(tbK.toString()));
+                    numIter = kmeans.kmeans(data);
+                    openDialog("K-MEANS", "Numero di iterazioni: " + numIter + "\n" + kmeans.getC().toString(data));
                 }
-            }
-        });
-
-        btCalc.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                int numIter;
-                try {
-                    if (tbK.toString().equals("")) {
-                        openDialog("ERRORE", "Inserire il numero di cluster!");
-                    } else {
-                        kmeans = new KMeansMiner(Integer.parseInt(tbK.toString()));
-                        numIter = kmeans.kmeans(data);
-                        openDialog("K-MEANS", "Numero di iterazioni: " + numIter + "\n" + kmeans.getC().toString(data));
-                    }
-                } catch (OutOfRangeSampleSize e) {
-                    openDialog("ERRORE", e.getMessage());
-                }
+            } catch (OutOfRangeSampleSize e) {
+                openDialog("ERRORE", e.getMessage());
             }
         });
     }
