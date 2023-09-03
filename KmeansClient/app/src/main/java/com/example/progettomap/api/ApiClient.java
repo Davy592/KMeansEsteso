438dc6bd99250a5b1bfae621656b3f76c9ed3bff
mattiacurri <mattia2002@gmail.com>
2023-09-03 16:41:30 +0200

    Rimossi import inutilizzati

--- KmeansClient/app/src/main/java/com/example/progettomap/api/ApiClient.java
+++ KmeansClient/app/src/main/java/com/example/progettomap/api/ApiClient.java
@@ -1,11 +1,8 @@
 package com.example.progettomap.api;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
