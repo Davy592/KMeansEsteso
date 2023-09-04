8fa2964d152ba974d6a5e636bd5fc0de5ace7a96
Davide Cirilli <davide.cirilli592@gmail.com>
2023-09-04 12:32:08 +0200

    Modificato albero package e javadoc

---
+++ KmeansClient/app/src/main/java/com/activity/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Il package activity contiene le classi che gestiscono le activity dell'app.
+ */
+package com.activity;
---
+++ KmeansClient/app/src/test/java/com/test/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Il package test contiene i test dell'app.
+ */
+package com.test;
