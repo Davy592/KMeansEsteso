d9c6c37af3a68646da085da492054d90cc449442
Davide Cirilli <davide.cirilli592@gmail.com>
2023-09-05 14:49:44 +0200

    Compilato con android 14 (sdk 34)

--- KmeansClient/app/build.gradle.kts
+++ KmeansClient/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.progettomap"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.progettomap"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
